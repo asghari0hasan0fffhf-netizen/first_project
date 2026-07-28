5b2fa3c355b520ec2a3ac543c363bee617f6ae32
hassan <asghari0hasan0fffhf@gmail.com>
2026-07-28 09:55:15 +0430

    First commit

---
+++ first.dart
@@ -0,0 +1,15 @@
+class Car {
+  int? speed;
+  String? color;
+  String? pelak;
+
+  
+}
+
+class Characte{
+  int? id;
+  String? name;
+  void enter() {
+    print('entering ....');
+  }
+}
---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hello');
+  print('Ali');
+}
+
+class Character {
+  String? name;
+  String? lastName;
+  String? colorBody;
+
+  void eat() {
+    print('eating...');
+  }
+}
